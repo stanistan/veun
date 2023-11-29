23ca88bbcdc25dce2036dec8295720abc26e6cec
Stan Rozenraukh <stan@stanistan.com>
2023-11-29 11:29:31 -0500

    Initial test for rendering PersonView(Person...)

---
+++ render_person_test.go
@@ -0,0 +1,41 @@
+package veun_test
+
+import (
+	"html/template"
+	"testing"
+
+	"github.com/alecthomas/assert/v2"
+	"github.com/stanistan/veun"
+)
+
+type Person struct {
+	Name string
+}
+
+type personView struct {
+	Person Person
+}
+
+func PersonView(person Person) *personView {
+	return &personView{Person: person}
+}
+
+var _ veun.Renderable = &personView{}
+
+var personViewTpl = template.Must(
+	template.New("PersonView").Parse(`<div>Hi, {{ .Name }}.</div>`),
+)
+
+func (v *personView) Template() (*template.Template, error) {
+	return personViewTpl, nil
+}
+
+func (v *personView) TemplateData() (any, error) {
+	return v.Person, nil
+}
+
+func TestRenderPerson(t *testing.T) {
+	html, err := veun.Render(PersonView(Person{Name: "Stan"}))
+	assert.NoError(t, err)
+	assert.Equal(t, html, template.HTML(`<div>Hi, Stan.</div>`))
+}
