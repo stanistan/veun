cde937033d46b06c8fb959e745df8d73436c4267
Stan Rozenraukh <stan@stanistan.com>
2023-12-04 15:00:40 -0500

    tempalte FS parsing

--- view.go
+++ view.go
@@ -3,6 +3,7 @@ package veun
 import (
 	"context"
 	"html/template"
+	"io/fs"
 )
 
 type View struct {
@@ -27,10 +28,16 @@ func slotFuncStub(name string) (template.HTML, error) {
 	return template.HTML(""), nil
 }
 
+func newTemplate(name string) *template.Template {
+	return template.New(name).Funcs(template.FuncMap{
+		"slot": slotFuncStub,
+	})
+}
+
 func MustParseTemplate(name, contents string) *template.Template {
-	return template.Must(
-		template.New(name).
-			Funcs(template.FuncMap{"slot": slotFuncStub}).
-			Parse(contents),
-	)
+	return template.Must(newTemplate(name).Parse(contents))
+}
+
+func MustParseTemplateFS(f fs.FS, ps ...string) *template.Template {
+	return template.Must(newTemplate("ROOT").ParseFS(f, ps...))
 }
